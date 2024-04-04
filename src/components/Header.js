52dc5f2a3d8f75fb49653666b5aac34ce0b84e2b
Harriet Oteng <harriet.oteng@gmail.com>
2024-04-04 17:22:07 +0000

    Update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { VscBug } from "react-icons/vsc";
+import {
+  PiChatTeardropDotsLight, PiBellLight, PiCaretDownLight, PiMagnifyingGlassLight,
+} from 'react-icons/pi';
 import { IoIosArrowDown } from "react-icons/io";
 import CompanyLogo from "../assets/favicon.webp"
 
@@ -23,34 +26,34 @@ const Header = () => {
 
   // Style for active link
   const navLinkStyles = ({ isActive }) => ({
-    fontWeight: isActive ? '700' : '500',
+    color: isActive ? '#0B6CB4' : '',
   });
 
   // max-w-[1504px]
   return (
     <>
-      <header className="bg-gray-200 fixed w-full z-[10]">
-        <div className="flex justify-between items-center h-[55px] my-0 mx-auto px-2 xl:px-8 py-0 ">
+      <header className="bg-white fixed w-full z-[10] shadow-xl">
+        <div className="text-[15px] h-[107px] flex justify-between items-center my-0 mx-auto px-2 xl:px-8 py-0 ">
           <div className="flex items-center justify-center xl:gap-6 md:gap-2">
             <div className="w-[40px] h-[40px] flex items-center justify-center">
               <img className="" src={CompanyLogo} alt="company logo" />
             </div>
-            <nav className="px-3 h-[40px] rounded-full border-2 border-accentmagenta flex justify-center items-center gap-3">
-              <NavLink style={navLinkStyles} to="/">Equity</NavLink>
-              <NavLink style={navLinkStyles} to="/fixedincome">Fixed Income</NavLink>
-              <NavLink style={navLinkStyles} to="/myaccounts">My Accounts</NavLink>
-              <NavLink style={navLinkStyles} to="/newsroom">Newsroom</NavLink>
-              <NavLink style={navLinkStyles} to="/research">Research</NavLink>
-              <NavLink style={navLinkStyles} to="/marketdata">Market Data</NavLink>
-              <NavLink style={navLinkStyles} to="/analytics">Analytics</NavLink>
-              <NavLink style={navLinkStyles} to="/liquidity">Liquidity</NavLink>
+            <nav className="text-[#032E42] px-3 h-[40px] flex justify-center items-center gap-4 font-semibold">
+              <NavLink className="text-center w-min hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/">EQUITY</NavLink>
+              <NavLink className="text-center w-min xl:w-max hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/fixedincome">FIXED INCOME</NavLink>
+              <NavLink className="text-center w-min xl:w-max hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/myaccounts">MY ACCOUNTS</NavLink>
+              <NavLink className="text-center w-min hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/newsroom">NEWSROOM</NavLink>
+              <NavLink className="text-center w-min hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/research">RESEARCH</NavLink>
+              <NavLink className="text-center w-min xl:w-max hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/marketdata">MARKET DATA</NavLink>
+              <NavLink className="text-center w-min hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/analytics">ANALYTICS</NavLink>
+              <NavLink className="text-center w-min hover:text-[color:#0B6CB4]" style={navLinkStyles} to="/liquidity">LIQUIDITY</NavLink>
             </nav>
           </div>
-          <div className="flex items-center justify-center xl:gap-3 md:gap-2 border relative">
-            <button className="xl:mr-[12px] flex items-center justify-center px-2 bg-accentmagenta text-white rounded-md h-[40px]">Request LP</button>
-            <button className="bg-white text-lg rounded-full flex items-center justify-center w-[40px] h-[40px]"><VscBug /></button>
-            <button className="bg-white text-xl rounded-full flex items-center justify-center w-[40px] h-[40px]"><IoIosNotificationsOutline /></button>
-            <button onClick={updateUser} className="bg-white rounded-full flex items-center justify-center w-[40px] h-[40px] relative"><span>HO</span><IoIosArrowDown className="absolute w-[40px] bottom-[-1px] text-gray-400" /></button>
+          <div className="flex items-center justify-center xl:gap-3 md:gap-3">
+            <button className="flex items-center justify-center px-3 h-[35px] border-2 border-[#0B6CB4] text-[#0B6CB4] hover:text-white hover:bg-[#0B6CB4] rounded-md text-[16px] w-max">Request LP</button>
+            <button className="text-lg"><VscBug /></button>
+            <button className="text-xl"><IoIosNotificationsOutline /></button>
+            <button onClick={updateUser} className="flex items-center justify-center gap-[2px]"><span className="w-[35px] h-[35px] rounded-full flex items-center justify-center  border-[0.5px] border-[#606060]" >HO</span><PiCaretDownLight className="text-lg" /></button>
             <div id="sign-out" className={`${signOutClass} bg-white h-[35px] px-2 absolute right-0 bottom-[-40px] flex items-center justify-center cursor-pointer`}>Sign Out</div>
           </div>
         </div>
@@ -61,3 +64,5 @@ const Header = () => {
 }
 
 export default Header
+
+// xl:mr-[12px]
